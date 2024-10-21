a6325eb68130b2f7028a03d58d511aa1fef61fdd
huangjian <huangjian@mingyangtech.com.cn>
2024-10-21 10:12:02 +0800

    init

---
+++ authorization.go
@@ -0,0 +1,59 @@
+package traefik_authorization_keycloak
+
+import (
+	"context"
+	"net/http"
+	"net/url"
+	"strings"
+)
+
+type Config struct {
+	KeycloakURL      string `json:"keycloak_url"`
+	KeycloakClientId string `json:"keycloak_client_id"`
+}
+
+type AuthMiddleware struct {
+	next             http.Handler
+	keycloakClientId string
+	keycloakUrl      string
+	name             string
+}
+
+func (am *AuthMiddleware) ServeHTTP(w http.ResponseWriter, req *http.Request) {
+	resourceUrl := req.URL.Path
+	method := req.Method
+	permission := resourceUrl + "#" + method
+
+	formData := url.Values{}
+	formData.Set("permission", permission)
+	formData.Set("grant_type", "urn:ietf:params:oauth:grant-type:uma-ticket")
+	formData.Set("audience", am.keycloakClientId)
+	kcReq, err := http.NewRequest("POST", am.keycloakUrl, strings.NewReader(formData.Encode()))
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusUnauthorized)
+	}
+	kcReq.Header.Set("Authorization", "Bearer "+req.Header.Get("X-Auth-Request-Access-Token"))
+	kcReq.Header.Set("Content-Type", "application/x-www-form-urlencoded")
+
+	client := &http.Client{}
+	kcResp, err := client.Do(kcReq)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusUnauthorized)
+	}
+	defer kcResp.Body.Close()
+
+	if kcResp.StatusCode == http.StatusOK {
+		am.next.ServeHTTP(w, req)
+	} else {
+		http.Error(w, "Unauthorized", http.StatusUnauthorized)
+	}
+}
+
+func New(ctx context.Context, next http.Handler, config *Config, name string) (http.Handler, error) {
+	return &AuthMiddleware{
+		next:             next,
+		name:             name,
+		keycloakUrl:      config.KeycloakURL,
+		keycloakClientId: config.KeycloakClientId,
+	}, nil
+}
---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/mingyangtech/traefik-authorization-keycloak
+
+go 1.23
